324b43d95b3e98cef682d9d5b813ab29da9c844d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2022-08-11 03:37:48 +0100

    Imported SafeMath for fees

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -3,10 +3,14 @@ import "./NFT.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract NFTMarket is ReentrancyGuard{
+contract NFTMarket is ReentrancyGuard,Ownable{
     using Counters for Counters.Counter;
+    using SafeMath for uint;
     Counters.Counter private _marketId;
+    uint listingFee = 0.05 ether;
     
        struct Listing{
         address NFTAddr;
@@ -21,9 +25,11 @@ contract NFTMarket is ReentrancyGuard{
        }
 
        mapping(uint => Listing)listing;
+       mapping(uint => address)IDListing;
        mapping(address => uint)auction;
 
-    function ListNFT(address _nftaddr,uint _price,uint _tokenid,uint _duration) nonReentrant public{
+    function ListNFT(address _nftaddr,uint _price,uint _tokenid,uint _duration) nonReentrant public payable{
+        require(msg.value == listingFee,"Pay 0.05 ether to list item");
         require(NFT(_nftaddr).IsNFTOwner(msg.sender,_tokenid) == true,"You do not own the NFT");
         _marketId.increment();
         uint id = _marketId.current();
@@ -32,9 +38,15 @@ contract NFTMarket is ReentrancyGuard{
         listing[id].tokenId = _tokenid;
         listing[id].duration = _duration;
         listing[id].seller = payable(msg.sender);
+        IDListing[id] = msg.sender;
         IERC721(_nftaddr).transferFrom(msg.sender,address(this),_tokenid);
     }
-    function BuyNFT()public{
+
+    function BuyNFT(uint _marketID)public payable{
+        require(msg.value == listing[_marketID].price,"Pay Item price");
+        require(listing[_marketID].seller != address(0),"Item does not exist");
+        require(listing[_marketID].sold == false,"Item has been bought");
+        require(listing[_marketID].canceled == false,"Item has been retrieved by seller");
 
     }
 
@@ -48,6 +60,13 @@ contract NFTMarket is ReentrancyGuard{
     _buyer = listing[_marketID].buyer;
     _sold = listing[_marketID].sold;
     _canceled = listing[_marketID].canceled;
+    }
+    function CancelListing(uint _marketID,address _nftaddr)public{
+        require(listing[_marketID].seller != address(0),"Item does not exist");
+        require(IDListing[_marketID] == msg.sender,"You are not the Lister");
+        uint tokenId = listing[_marketID].tokenId;
+        listing[_marketID].canceled = true;
+        IERC721(_nftaddr).transferFrom(address(this),msg.sender,tokenId);
 
     }
     
